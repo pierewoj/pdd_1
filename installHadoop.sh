271d7476a1db466dfdb3fb0fc941c836c65786e2
Jakub Pierewoj <jp360641@students.mimuw.edu.pl>
2016-10-24 23:15:34 +0200

    skrypty

---
+++ installHadoop.sh
@@ -0,0 +1,52 @@
+rm -rf /tmp/hadoop
+mkdir /tmp/hadoop
+tar -xf ~/Download/hadoop-2.7.2.tar.gz -C /tmp/hadoop
+mv /tmp/hadoop/hadoop-2.7.2/* /tmp/hadoop
+rmdir /tmp/hadoop/hadoop-2.7.2
+export HADOOP_PREFIX=/tmp/hadoop/
+sed -i -e "s|^export JAVA_HOME=\${JAVA_HOME}|export JAVA_HOME=/usr/lib64/jvm/java|g" ${HADOOP_PREFIX}/etc/hadoop/hadoop-env.sh
+cd $HADOOP_PREFIX
+
+cp ~/Dokumenty/pdd/masters $HADOOP_PREFIX/etc/hadoop/masters
+cp ~/Dokumenty/pdd/slaves $HADOOP_PREFIX/etc/hadoop/slaves
+
+cat <<EOF > ${HADOOP_PREFIX}/etc/hadoop/core-site.xml
+<configuration>
+<property>
+<name>fs.defaultFS</name>
+<value>hdfs://`cat ~/Dokumenty/pdd/masters`:9000</value>
+</property>
+</configuration>
+EOF
+
+cat <<EOF > ${HADOOP_PREFIX}/etc/hadoop/hdfs-site.xml
+<configuration>
+<property>
+<name>dfs.replication</name>
+<value>1</value>
+</property>
+</configuration>
+EOF
+
+cat <<EOF > ${HADOOP_PREFIX}/etc/hadoop/mapred-site.xml
+<configuration>
+<property>
+<name>mapreduce.framework.name</name>
+<value>yarn</value>
+</property>
+
+  <property>
+      <name>mapred.child.java.opts</name>
+          <value>-Xmx1024m</value>
+	    </property>
+</configuration>
+EOF
+
+cat <<EOF > ${HADOOP_PREFIX}/etc/hadoop/yarn-site.xml
+<configuration>
+<property>
+<name>yarn.nodemanager.aux-services</name>
+<value>mapreduce_shuffle</value>
+</property>
+</configuration>
+EOF
---
+++ startHadoop.sh
@@ -0,0 +1,13 @@
+while read slave
+do
+	echo installing hadoop on "$slave"
+	ssh jp360641@$slave 'bash -s' < installHadoop.sh
+
+done < slaves
+export _JAVA_OPTIONS=-Xmx6000m
+/tmp/hadoop/sbin/stop-dfs.sh
+/tmp/hadoop/bin/hdfs namenode -format
+/tmp/hadoop/sbin/start-dfs.sh
+/tmp/hadoop/bin/hdfs dfs -mkdir /user
+/tmp/hadoop/bin/hdfs dfs -mkdir /user/jp360641
+/tmp/hadoop/bin/hdfs dfs -ls
